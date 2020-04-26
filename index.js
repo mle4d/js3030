3772eebabb46ae7eb4e5d8d14a00849290bf55c6
emily <mle4d@me.com>
2020-04-26 16:45:13 -0700

    tweak pop up time & game time

---
+++ index.js
@@ -0,0 +1,48 @@
+const bricks = document.querySelectorAll('.brick');
+  const scoreBoard = document.querySelector('.scorenum');
+  const minifigs = document.querySelectorAll('.minifig');
+  let lastbrick;
+  let timeUp = false;
+  let scorenum = 0;
+
+  function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+  function randombrick(bricks) {
+    const idx = Math.floor(Math.random() * bricks.length);
+    const brick = bricks[idx];
+    if (brick === lastbrick) {
+      console.log('Ah nah thats the same one bud');
+      return randombrick(bricks);
+    }
+    lastbrick = brick;
+    return brick;
+  }
+
+  function peep() {
+    const time = randomTime(500, 1200);
+    const brick = randombrick(bricks);
+    brick.classList.add('up');
+    setTimeout(() => {
+      brick.classList.remove('up');
+      if (!timeUp) peep();
+    }, time);
+  }
+
+  function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 30000)
+  }
+
+  function bonk(e) {
+    if(!e.isTrusted) return; // cheater!
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+  }
+
+  minifigs.forEach(minifig => minifig.addEventListener('click', bonk));
